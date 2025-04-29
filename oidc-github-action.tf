0200c9e2e438a52782abdb122c732b12aba1b4c8
David Joo <david.joo@hashicorp.com>
2025-04-29 12:22:29 +1000

    Update OIDC policy to restrict access to all repositories in the organization

--- oidc-github-action.tf
+++ oidc-github-action.tf
@@ -51,7 +51,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_org}/${var.github_repo}:*"] # Restrict to your repo
+      values   = ["repo:${var.github_org}/*"] # Restrict to your repo
     }
   }
 }
